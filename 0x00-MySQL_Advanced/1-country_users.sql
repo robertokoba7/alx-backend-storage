85fbbef5d7d11ccf33311e6196efbd253a610c10
robertokoba7 <okobarobert7@gmail.com>
2023-04-06 14:05:24 +0000

    [add] enumaration of countries is user table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,4 @@
--- SQl script that creates a table `users`
-
+-- SQl script that adds a column to a table `users`
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
