a8404f71318f8892b65432a82199d01c860f0c76
robertokoba7 <okobarobert7@gmail.com>
2023-04-06 20:17:33 +0000

    [add] quantity decreases on new orders

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,5 +4,6 @@ CREATE TRIGGER decrease_quantity
 BEFORE INSERT ON orders 
 FOR EACH ROW UPDATE items
     -- Less the quantity of the item following the order from the 'quantity' column in the 'items' table
-SET quantity = quantity - NEW.quantity WHERE id = NEW.item_id;
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name
 END;
