e44e7938c8786b5170acb8499826d92b87d57326
robertokoba7 <okobarobert7@gmail.com>
2023-04-06 11:35:57 +0000

    [add]Create a table `user` and  add the given attributes && git push origin main

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- SQl script that creates a table users
+-- SQl script that creates a table `users`
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
