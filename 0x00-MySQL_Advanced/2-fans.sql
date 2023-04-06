a9fd2ad40525b3122229cbec0ccd9b09ab9e11cf
robertokoba7 <okobarobert7@gmail.com>
2023-04-06 19:45:53 +0000

    [add] ranking country origins of bands, ordered by the number of (non-unique) fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- Calculate the total number of fans for each country of origin
-SELECT origin, SUM(nb_fans) AS total_fans 
-FROM metal_bands
+SELECT DISTINCT origin, SUM(fans) AS nb_fans FROM metal_bands
 -- Group the bands by their country of origin
 GROUP BY origin
 -- Order the results by the total number of fans in descending order
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
