551ea51baca54a3919aeb4ac0a47beedfe1c71a7
robertokoba7 <okobarobert7@gmail.com>
2023-04-11 07:19:05 -0700

    [add]:  Insert a document

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,6 +4,6 @@
 
 def insert_school(mongo_collection, **kwargs):
     """function that inserts a new document in a collection based on kwargs"""
-    if len(kwags) == 0:
+    if len(kwargs) == 0:
         return None
     return mongo_collection.insert(kwargs)
